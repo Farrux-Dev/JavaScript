00ee907b2fcbc76f41f4bfe4da7399cca2acff9d
Farrux <Farrux@gmail.com>
2023-08-22 12:07:58 +0300

    JavaScript

---
+++ 2-dars/js/script.js
@@ -0,0 +1,13 @@
+//o'zgaruvchilar
+
+//let
+let number = 22; // bu let nomli o'zgaruvchu bu o'zgaruvchini qiymatini to'g'ridan to'g'ri o'zgartrsa bo'ladi;
+
+// const 
+const name = "Farrux" //bu const nomli o'zgaruvchi bu +ni qiymatini to'gridan to'g'ri o'zgartrib bo'lmaydi , lekin obect yordamida o'zgartrish mumkin albata misol uchun;
+
+const myname = {
+    name:"farrux"
+}
+myname.name = "Farrux_Dev"
+console.log(myname) //bu sizlarga tushunarli bo'lgan bo'lsa kerak , albatta const qiymatini o'zgartirish mumkin bu savol suhbatda tushushi mumkin eslab qoling ;
