491b210e3972743632d1fef3e36dcb7d5c33a82c
Farrux <Farrux@gmail.com>
2023-08-22 21:02:35 +0300

    JavaScript

---
+++ 4-dars/js/script.js
@@ -0,0 +1,24 @@
+// alert("hello alert") // bu oddiy alert hisoblanadi;
+
+// const help = confirm("how are you") // bu so'rov alert hisoblanadi buni true falsini ko'rsa bo'ladi;
+// console.log(help) // alertda ok bosilsa truega teng close bosilsa false ga teng bo'ladi;
+
+// const help = prompt("where are you from") // bu alertda siz nimadur yozishingiz mumkin bo'ladi buni qiymatini ham ko'rish mumkin;
+// console.log(help) // shunday yo'l bilan consolda ko'ring va ishalting agarda hech nima kiritmasdan close bosilsa null ga teng bo'ladi bu bizga har doim string qaytaradi ammo number qilsa ham bo'ladi ;
+
+// const ok = +prompt("bitinchi  sonni kiriting") // bu yerga son qo'yiladi 
+// const help = +prompt("ikkinchi sonni kiriting") // biz shunda yo'l bilan promptni oldiga + belgisini qo'ysak bu number qaytaradi bizga ;
+// console.log(help+ok) // shunday yo'l bilan ; va bunday qilib biz har hi mar=tematik amallar ishlasak ham bo'ladi;
+
+
+// const myColor = []  // bu yerda biz pasdagi promptga kiritlgan elementlarni shu massiv ichiga solayabmiz;
+
+
+// myColor[0] = prompt("favorite one color") // bu yerda birinchi rang kiritiladi va myColor nomli tepadagi bo'sh massivga 0 index bilan tushadi ;
+// myColor[1] = prompt("favorite two color") // bu yerda ikkinchi rang kiritiladi va myColor nomli massivga 1 index bilan tushadi chinki massiv lar har doim 0 dan boshlanadi;
+// console.log("my favoite color" , myColor) // bu yerda biz myColor ga kiritligan qiymatlarni console ga chiqarayabmiz;
+
+
+
+
+
---
+++ 5-dars/js/script.js
@@ -0,0 +1,6 @@
+// const url = prompt("who is Favourite youtuber") // bu yerda biz o'zimzga yoqqan youtubedagi bloger profilini yozamiz;
+// console.log("https://youtube.com/" + url) //bu yerada biz qo'shtirnoq ichida doman yozib yoniga tepadagi url o'zgaruvchisini qo'yamiz va consol bizga silka yasab beradi buni boshqa yo'o ham bor;
+
+
+// const url  = prompt("who is favourite youtuber") // bu yerda o'zimizga yoqqam youtuber bloger profilini yozaamiz;
+// console.log(`https://youtube.com/${url}`) // shunnday yo'l bilan ossonroq bo'ladi;
