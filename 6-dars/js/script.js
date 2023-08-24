3751cba41dcbe12cf07b9e21a053bf9201674e65
Farrux <Farrux@gmail.com>
2023-08-24 15:24:00 +0300

    JavaScript

---
+++ 6-dars/js/script.js
@@ -0,0 +1,28 @@
+//  + va  -
+
+let incr = 10; // bu yerda biz incr nomlii o'zgaruvchini 10 ga tenglayabmiz incr degani oldinga qo'shuvchi degam manoda keladi;
+let decr = 10; // bu yerda biz decr nomli o'zgaruvchini 10 ga tenglayabmiz decr degani orqaga ayitivchi degan manoda keladi;
+
+incr++; //bu yeda biz incr nomli fubksiyaga qoshayabmiz 2ta ++ har doim 1 ga oshiradi;
+decr--; // bu yeda biz decr nomloi funksiydan ayirayabmiz 2ta -- har doim 1 taga ayiradi;
+console.log(incr, decr) // bu yerda biz incr va decr ni console da chaqirayabmiz;   
+// protsent
+console.log(10 % 3) // bu protsent qoldiqli bo'ish hisoblanadi; bu ham kerak bo'ladi unutmang;
+
+ // tenglik belgilari
+console.log(4*4 ==16) // bu yerda tenglashtrish belgisini qo'yilgan 2 teng hardoim tenglashtrish uchun ishlatiladi ; 2 tang qiymati bo'yicha tenglashtiradi;
+
+console.log(4*4 ==="16") // bu yerda ham tenglash trish belgisi qo'yilgan 2 teng qiymati bo'yicha hisoblaydi, 3 ta teng esa ham qiymati ham malumot turi bo'yicha hioblaydi;
+
+
+//  &&=va,  || = yoki  
+
+const myname = true;
+const younname = true;
+const wename = true;
+
+console.log(myname && younname && wename) // bu yerda biz tepadagi o'zgaruvchuilarning va qilib consoleda chiqarayabmiz && bu belgu true chiqishu uchun hamma o'zgarubchilar true bo'oshi shat bo'lmasa false qaytaradi;
+
+console.log(myname || younname || wename) // bu yerda biz tepadagi o'zgaruvchilarni yoki qilib consoleda chiqarayabmiz || bu belgi true chiqarishi uchun 11 true bo'lsa yetadi qolgan o'zgaruvchular false bo'lsada 1 ta true bo'lsa true qaytaradi;
+ 
+
