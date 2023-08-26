6a6536d40e52b31ec83f076fd42c3b7db7cd34f4
Farrux <Farrux@gmail.com>
2023-08-26 11:16:39 +0300

    JavaScript

---
+++ 9-dars/js/script.js
@@ -0,0 +1,22 @@
+// tsikls qamday dir jarayonda nimadur qilish qandaydir voqtda;
+
+
+let  Numb =100
+
+// while(Numb <= 100){ // bu yerda biz tapadagi Numb nomli o'zgaruvchini 100 gacha ishlasin deganbiz;
+// console.log(Numb);
+// Numb++; // bu yerda biz Numb ni har doim yuzgacha 1 ta bita oshirsin deganbiz;
+// }
+
+// do{
+// console.log(Numb) // bu yerda biz shart qo'yvattik shu yerda chiqsin va oshirsin deb
+// Numb++;
+// if(Numb ===12){
+//   break; // bu yerda biz Numb 12 ga kelganda toxtatib qoysin va boshqa songa o'tmasin deyabmiz; 
+// }
+// }while(Numb <=1200) // bu yerda biz nima bo'lishini yozvattik;
+
+// for(let i=1; i < 12; i++){ // bu yerda biz for dan foydalandik va shu yerni o'zida o'zgaruvchi ochib ishlatdik
+//     console.log(i)
+    
+// }
